e246d56efd61a575155ce18b1fde98587535bceb
hasashin <dominik.hazak@outlook.com>
2019-10-08 21:55:53 +0200

    Initial commit

---
+++ Szyfrator1/MainWindow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Gtk;
+using Szyfrator1;
+using System.Numerics;
+
+public partial class MainWindow : Gtk.Window
+{
+    private Przekatnokolumnowy szyfr;
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        szyfr = new Przekatnokolumnowy();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnCipherButtonClicked(object sender, EventArgs e)
+    {
+        szyfr.key = KeyCipherEntry.Text;
+        szyfr.Encrypt(InputRawText.Buffer.Text);
+    }
+    protected void OnDecipherButtonClicked(object sender, EventArgs e)
+    {
+        szyfr.key = KeyCipherEntry.Text;
+        szyfr.Encrypt(InputRawText.Buffer.Text);
+    }
+
+    protected void OnKeyCipherEntryChanged(object sender, EventArgs e)
+    {
+
+        double pow = Math.Pow(3.0, 127.0);
+        ulong wpizdu = 3 * Convert.ToUInt64(pow);
+        ulong wynik = wpizdu % 256;
+        
+        MessageDialog dlg = new MessageDialog(this, DialogFlags.Modal, MessageType.Other, ButtonsType.Ok, wynik.ToString());
+        dlg.Run();
+        dlg.Destroy();
+    }
+}
---
+++ Szyfrator1/Przekatnokolumnowy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Szyfrator1
+{
+    public class Przekatnokolumnowy
+    {
+        public string key { get; set; }
+
+        public Przekatnokolumnowy(){}
+
+        public Przekatnokolumnowy(string key)
+        {
+            this.key = key;
+        }
+
+        public string Encrypt(string data)
+        {
+            return "";
+        }
+        public string Decrypt(string data)
+        {
+            return "";
+        }
+    }
+}
